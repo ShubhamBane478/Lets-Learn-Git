2664f703c75b7b7b55bc553ac42d4c3b69bdfc2e
Shubham Bane <shubhambane435@gmail.com>
2023-07-03 23:00:55 +0530

    Mutiply Calc

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Shubham Bane")
+
+
+        // Multiply two numbers
+        const num1 = 5;
+        const num2 = 7;
+        const result = num1 * num2;
+
+        // Display the result on the page
+        const resultElement = document.getElementById('result');
+        resultElement.textContent = `The result of ${num1} * ${num2} is: ${result}`;
+    
